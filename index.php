2d3c371640ba410fef09332c7dade23b59cd9ba8
Tom Woodward <twwoodward@me.com>
2019-05-23 15:26:48 -0400

    hey! acf to plugin
    probably turn it off for production in multiste but helpful in the meantime

--- index.php
+++ index.php
@@ -62,3 +62,33 @@ function get_progress_by_user(){
 	endif;
 
 }
+
+//ACF JSON SAVER
+add_filter('acf/settings/save_json', 'my_acf_json_save_point');
+ 
+function my_acf_json_save_point( $path ) {
+    
+    // update path
+    $path = plugin_dir_path( __FILE__ )  . '/acf-json';
+    // return
+    return $path;
+    
+}
+
+
+add_filter('acf/settings/load_json', 'my_acf_json_load_point');
+
+function my_acf_json_load_point( $paths ) {
+    
+    // remove original path (optional)
+    unset($paths[0]);
+    
+    
+    // append path
+    $paths[] = plugin_dir_path( __FILE__ )  . '/acf-json';
+    
+    
+    // return
+    return $paths;
+    
+}
